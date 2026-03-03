87f8a971cf5404a1d2e5a76aaf65edab50f53d8b
pauluss-arch <paulussiagian227@gmail.com>
2026-03-03 14:22:28 +0700

    pretest

---
+++ src/Pretest.java
@@ -0,0 +1,5 @@
+public class Pretest {
+    public static void main(String[] args) throws Exception {
+        System.out.println("========================================\nNama\t: Paulus Roberto Apolos Siagian\nNIM\t: 105224020\nProdi\t: Ilmu komputer\n========================================");
+    }
+}
